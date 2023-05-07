dd9ddb5ae90e7bc87bd4f09df3401e53162b487a
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-07 22:27:56 +0300

    add style and component

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const TodoList = () => {
+  return <main className={styles.todocontainer}>
+
+    <p className={styles.pContainer}>Todo List</p>
+
+  </main>;
+};
+
+export default TodoList;
