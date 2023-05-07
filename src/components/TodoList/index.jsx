467dcc69a594c62eb9c8a36aaf19d447289dbb3c
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-07 23:58:49 +0300

    refactor style

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -5,11 +5,6 @@ const task = [
   { id: 1, task: "Eat", status: false },
   { id: 2, task: "Code", status: false },
   { id: 3, task: "Review", status: false },
-  {
-    id: 4,
-    task: "Drink fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffcoffee",
-    status: false,
-  },
 ];
 
 const TodoList = () => {
@@ -17,7 +12,7 @@ const TodoList = () => {
     <li key={value.id} className={styles.liTask}>
       <p className={styles.pTask}>{value.task}</p>
       <div>
-        <input type="checkbox" />
+        <input type="checkbox" value={true} />
         <button className={styles.btnDel}>Delete</button>
       </div>
     </li>
@@ -27,7 +22,11 @@ const TodoList = () => {
     <main className={styles.todocontainer}>
       <p className={styles.pContainer}>Todo List</p>
       <div className={styles.containerTask}>
-        <input className={styles.inputTask} placeholder="Input task" />
+        <input
+          className={styles.inputTask}
+          placeholder="Input task"
+          type="text"
+        />
         <button className={styles.btnAddTask}>Add Task</button>
       </div>
       <ul>{taskList}</ul>
