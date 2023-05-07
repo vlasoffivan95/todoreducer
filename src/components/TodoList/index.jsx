97ab513a67aad64df96fa5826a0190b053f939ec
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-08 01:31:07 +0300

    add slicer

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { addTask } from "../../store/slices/todoSlice";
 
 const task = [
   { id: 1, task: "Eat", status: false },
@@ -7,7 +9,14 @@ const task = [
   { id: 3, task: "Review", status: false },
 ];
 
-const TodoList = () => {
+const TodoList = (props) => {
+  const taskLists = useSelector(state=>state.todo)
+  const dispatch=useDispatch()
+// const addTaskNew = (newTask)=>dispatch(addTask(newTask))
+
+
+  console.log(taskLists)
+
   const taskList = task.map((value, index) => (
     <li key={value.id} className={styles.liTask}>
       <p className={styles.pTask}>{value.task}</p>
@@ -26,6 +35,7 @@ const TodoList = () => {
           className={styles.inputTask}
           placeholder="Input task"
           type="text"
+
         />
         <button className={styles.btnAddTask}>Add Task</button>
       </div>
---
+++ src/store/slices/todoSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { v4 as uuid } from 'uuid'
+
+const initialState = {
+    todos:[]
+}
+
+
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {
+        addTask: (state, action) => {
+            const todo = {
+                id: uuid(),
+                task: action.payload
+            }
+
+            state.todos.push(todo)
+
+        }
+        
+    }
+
+})
+
+
+
+const { reducer, actions } = taskSlice;
+const { addTask } = actions;
+
+
+export default reducer;
+export { addTask }
