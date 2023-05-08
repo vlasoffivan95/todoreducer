57caf64352bf5fb4970c05a729e5b5a654769f21
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-09 00:03:29 +0300

    -

--- src/store/slices/todoSlice.js
+++ src/store/slices/todoSlice.js
@@ -13,7 +13,7 @@ const taskSlice = createSlice({
         addTask: (state, action) => {
             const todo = {
                 id: uuid(),
-                task: action.payload.trim(),
+                task: action.payload,
                 status: true,
             }
 
